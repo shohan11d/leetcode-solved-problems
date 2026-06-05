174f50bdba333c1f55da43c9f9faaaf21d836eda
shohan11d <shohan11d@gmail.com>
2026-06-05 13:24:37 +0600

    problem sol added

--- 1560A.cpp
+++ 1560A.cpp
@@ -2,26 +2,32 @@
 using namespace std;
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-  int t;
-  cin >> t;
+    int t;
+    cin >> t;
 
-  while (t--) {
-    cout << endl << "test " << 10 - t << " starts" << endl;
+    while (t--) {
+        int k;
+        cin >> k;
 
-    int x;
-    cin >> x;
-    int sum = 1;
-    int el = 1;
+        int element = 0;
+        int sum = 1;
 
-    while (el <= x) {
-      cout << "el:" << el << endl;
-      el++;
+        while (true) {
+            if (sum % 3 != 0 && sum % 10 != 3) {
+                element++;
+
+                if (element == k) {
+                    cout << sum << '\n';
+                    break;
+                }
+            }
+
+            sum++;
+        }
     }
-    cout << endl << "test " << 10 - t << " ends" << endl;
-  }
 
-  return 0;
+    return 0;
 }
