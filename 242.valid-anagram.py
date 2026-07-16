a6f525c222a666aa8f50384e5577dbf659605286
shohan11d <shohan11d@gmail.com>
2026-07-16 15:35:32 +0600

    revamped the entire folders

---
+++ 242.valid-anagram.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=242 lang=python3
+#
+# [242] Valid Anagram
+#
+
+# @lc code=start
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return sorted(s) == sorted(t) 
+        
+# @lc code=end
+
