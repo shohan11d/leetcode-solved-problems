b51c1d2cf2dd69a34a395c1cad4bd463ca9a069d
shohan11d <shohan11d@gmail.com>
2026-07-19 13:14:35 +0600

    two sum updated

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -7,12 +7,14 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[j] == target - nums[i]:
-                    return [i, j]
-        # Return an empty list if no solution is found
-        return []
+       for i in range(len(nums)):
+           for j in range(i+1, len(nums)):
+               if nums[i]+nums[j]==target:
+                   return [i,j]
+        
+        
+        
+
         
 # @lc code=end
 
---
+++ 231.power-of-two.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=231 lang=python3
+#
+# [231] Power of Two
+#
+
+# @lc code=start
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        
+# @lc code=end
+
