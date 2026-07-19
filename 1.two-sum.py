f76e731b994a5fbb7118e5e4247a7b08f43e4305
shohan11d <shohan11d@gmail.com>
2026-07-19 13:34:02 +0600

    two sum solution updated using hash table

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -6,13 +6,17 @@
 
 # @lc code=start
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-       for i in range(len(nums)):
-           for j in range(i+1, len(nums)):
-               if nums[i]+nums[j]==target:
-                   return [i,j]
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        prevMap = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+
+    
         
-        
+
         
 
         
