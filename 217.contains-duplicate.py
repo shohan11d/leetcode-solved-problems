7f30135ecd2c7ca9e7fb95bbb73a6385c1f6365e
shohan11d <shohan11d@gmail.com>
2026-08-13 10:36:36 +0600

    contains duplicate 3 solutions added

--- 217.contains-duplicate.py
+++ 217.contains-duplicate.py
@@ -5,13 +5,24 @@
 #
 
 # @lc code=start
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         nums.sort()
+#         for i in range(len(nums) - 1):
+#             if nums[i] == nums[i + 1]:
+#                 return True
+#         return False
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums.sort()
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i + 1]:
+        hash_set = set()
+
+        for num in nums:
+            if num in hash_set:
                 return True
+            hash_set.add(num)
         return False
+       
         
 # @lc code=end
 
