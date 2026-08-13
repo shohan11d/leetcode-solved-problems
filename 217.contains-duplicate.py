7a0bc82ceaf7be6d62db77f0167c6784b29fe63a
shohan11d <shohan11d@gmail.com>
2026-08-13 10:37:40 +0600

    contains duplicate 3 solutions added

--- 217.contains-duplicate.py
+++ 217.contains-duplicate.py
@@ -4,15 +4,42 @@
 # [217] Contains Duplicate
 #
 
-# @lc code=start
-# class Solution:
-#     def containsDuplicate(self, nums: List[int]) -> bool:
-#         nums.sort()
-#         for i in range(len(nums) - 1):
-#             if nums[i] == nums[i + 1]:
-#                 return True
-#         return False
 
+# ########### Brute Force ######################
+# Pattern: Two nested loops
+# Time: O(n²)
+# Space: O(1)
+# ###############################################
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+
+
+# ########### Sort + Check #####################
+# Pattern: Sorting + Adjacent comparison
+# Time: O(n log n)
+# Space: O(1)*  (depends on sorting implementation)
+# ###############################################
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+
+        return False
+
+
+# ########### Hash Set #########################
+# Pattern: Hash Set / Seen elements
+# Time: O(n) average
+# Space: O(n)
+# ###############################################
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         hash_set = set()
@@ -21,8 +48,8 @@ class Solution:
             if num in hash_set:
                 return True
             hash_set.add(num)
+
         return False
-       
         
 # @lc code=end
 
