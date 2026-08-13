0247db2f08929bd2b6b57093ced6ca37b8e78383
shohan11d <shohan11d@gmail.com>
2026-08-13 11:04:32 +0600

    valid anagrams solutions added

--- 242.valid-anagram.py
+++ 242.valid-anagram.py
@@ -5,6 +5,13 @@
 #
 
 # @lc code=start
+
+
+# ########### Sorting ##########################
+# Pattern: Sorting + Comparison
+# Time: O(n log n)
+# Space: O(n)*  (depends on sorting implementation)
+# ###############################################
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -13,6 +20,11 @@ class Solution:
         return sorted(s) == sorted(t)
 
 
+# ########### Hash Map #########################
+# Pattern: Frequency Counter / Hash Map
+# Time: O(n)
+# Space: O(n)
+# ###############################################
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -21,12 +33,13 @@ class Solution:
         countS, countT = {}, {}
 
         for i in range(len(s)):
-            countS[s[i]] = 1+ countS.get(s[i],0)
-            countS[t[i]] = 1+ countT.get(s[i],0)
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
         return countS == countT
 
 
-
+# @lc code=end
 
             
 
